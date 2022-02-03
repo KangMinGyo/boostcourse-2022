14914d27c03c136e063bd6e6b6865919a7381884
KangMinGyo <kang15567@kakao.com>
2022-02-03 21:05:55 +0900

    design: Main화면 UI 수정

---
+++ Album/CollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.swift
+//  Album
+//
+//  Created by Kang on 2022/02/03.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var albumImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var numLabel: UILabel!
+    
+}
--- Album/ViewController.swift
+++ Album/ViewController.swift
@@ -7,11 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDataSource {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    var numberOfCell: Int = 10
+    let cellIdentifier: String = "cell"
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return numberOfCell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath)
+        
+//        cell.nameLabel.text = "연필"
+        
+        return cell
+    }
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let flowLayout: UICollectionViewFlowLayout
+        flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets.zero
+        flowLayout.minimumInteritemSpacing = 10
+        flowLayout.minimumLineSpacing = 10
+
+        let halfWidth: CGFloat = UIScreen.main.bounds.width / 2.0
+        flowLayout.estimatedItemSize = CGSize(width: 90, height: 90)
+        self.collectionView.collectionViewLayout = flowLayout
     }
 
 
